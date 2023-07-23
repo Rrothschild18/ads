1dbfc9e348f31e3e2f665c02515b023f8b62cc76
rrothschild18 <raultonello18@gmail.com>
2023-07-23 18:34:54 -0300

    partial done

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import "./style.scss";
+import "./node_modules/jquery/dist/jquery";
+import "./node_modules/owl.carousel/dist/owl.carousel.js";
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 32,
+    dots: true,
+    dotClass: "custom-dot",
+    autoWidth: true,
+    dotsEach: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
